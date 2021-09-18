7f44ce5b40de16c697ff88a9744e116544cd5227
sallminrexha <you@example.com>
2021-09-18 11:19:13 +0200

    Conversion

--- src/SkeletonClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Sallmin\PhpPackage;
-
-class PhpPackageClass
-{
-}
---
+++ tests/WeightTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sallmin\PhpPackage\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sallmin\PhpPackage\Weight;
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_kilograms_to_lbs()
+    {
+        $lbs = Weight::fromKilograms(100)->toLbs();
+        $this->assertEquals(220.4623, $lbs);
+    }
+}
